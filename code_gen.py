94ab1d683f70bb5312eb46b865e16f1ccb882d06
Vishruth Devan <vishruthdevan@gmail.com>
2023-01-01 12:43:22 +0530

    feat: generate the map

--- code_gen.py
+++ code_gen.py
@@ -1,3 +1,6 @@
+import json
+
+
 def code_gen(characters):
     code = {}
     number = 0
@@ -5,3 +8,6 @@ def code_gen(characters):
         code[i] = str(number).zfill(3)
         number += 1
     return code
+
+
+print(code_gen(json.loads(open('characters.json').read())))
